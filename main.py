cccc3221fefd33f024fc98c7974841ebe08ac0cf
Jeline2 <jelineshyju5@gmail.com>
2025-08-12 21:32:11 +0530

    Lesson 6 Activity 2

---
+++ main.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().bgcolor("Blue")
+board=turtle.Turtle()
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.penup()
+board.right(150)
+board.forward(50)
